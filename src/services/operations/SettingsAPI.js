1e25d7309bd6634bdf72626f1bb90c8b311e07ab
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-07-27 22:12:35 +0530

    Update SettingsAPI.js

--- src/services/operations/SettingsAPI.js
+++ src/services/operations/SettingsAPI.js
@@ -14,7 +14,7 @@ const {
 
 export function updateDisplayPicture(token, formData) {
   return async (dispatch) => {
-    const toastId = toast.loading("Loading...")
+    const toastId = toast.loading("Loading..")
     try {
       const response = await apiConnector(
         "PUT",
