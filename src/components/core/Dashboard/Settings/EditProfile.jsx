3be090f3c62e8fb41a015397bf95248be5888a6c
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-02 01:01:19 +0530

    Update EditProfile.jsx

--- src/components/core/Dashboard/Settings/EditProfile.jsx
+++ src/components/core/Dashboard/Settings/EditProfile.jsx
@@ -6,7 +6,6 @@ import { updateProfile } from "../../../../services/operations/SettingsAPI"
 import IconBtn from "../../../common/IconBtn"
 
 const genders = ["Male", "Female", "Non-Binary", "Prefer not to say", "Other"]
-
 export default function EditProfile() {
   const { user } = useSelector((state) => state.profile)
   const { token } = useSelector((state) => state.auth)
