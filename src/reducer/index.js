22ea5066751753d5d04628944f054866f45c8738
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-02 02:03:25 +0530

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -5,7 +5,6 @@ import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice"
 import courseReducer from "../slices/courseSlice"
 import viewCourseReducer from "../slices/viewCourseSlice"
-
 const rootReducer  = combineReducers({
     auth: authReducer,
     profile:profileReducer,
