b2c4d9fb6709995ff2c4446a5fbf04f97b8e48f7
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-01 23:05:08 +0530

    Update Category.js

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -60,14 +60,12 @@ exports.categoryPageDetails = async (req, res) => {
         })
         .exec()
 
-      // Handle the case when the category is not found
       if (!selectedCategory) {
         console.log("Category not found.")
         return res
           .status(404)
           .json({ success: false, message: "Category not found" })
-      }
-      // Handle the case when there are no courses
+	  }
       if (selectedCategory.courses.length === 0) {
         console.log("No courses found for the selected category.")
         return res.status(404).json({
@@ -76,7 +74,6 @@ exports.categoryPageDetails = async (req, res) => {
         })
       }
   
-      // Get courses for other categories
       const categoriesExceptSelected = await Category.find({
         _id: { $ne: categoryId },
       })
@@ -89,8 +86,7 @@ exports.categoryPageDetails = async (req, res) => {
           match: { status: "Published" },
         })
         .exec()
-        //console.log("Different COURSE", differentCategory)
-      // Get top-selling courses across all categories
+   
       const allCategories = await Category.find()
         .populate({
           path: "courses",
@@ -104,7 +100,7 @@ exports.categoryPageDetails = async (req, res) => {
       const mostSellingCourses = allCourses
         .sort((a, b) => b.sold - a.sold)
         .slice(0, 10)
-       // console.log("mostSellingCourses COURSE", mostSellingCourses)
+      
       res.status(200).json({
         success: true,
         data: {
