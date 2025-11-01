d01181cbef7fdecaf322be4a176ae6d9b9bf54ed
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-01 23:46:16 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -15,8 +15,6 @@ exports.passwordUpdated = (email, name) => {
                 margin: 0;
                 padding: 0;
             }
-    
-    
             .container {
                 max-width: 600px;
                 margin: 0 auto;
