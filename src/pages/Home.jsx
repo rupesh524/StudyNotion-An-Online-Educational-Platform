01860051d5a81fbe4767e84689414b8118ebc19f
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-02 00:21:21 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
-// Icons Import
 import { FaArrowRight } from "react-icons/fa"
 import { Link } from "react-router-dom"
 
-// Image and Video Import
 import Banner from "../assets/Images/banner.mp4"
-// Component Imports
 import Footer from "../components/common/Footer"
 import ReviewSlider from "../components/common/ReviewSlider"
 import CTAButton from "../components/core/HomePage/Button"
@@ -18,9 +15,7 @@ import TimelineSection from "../components/core/HomePage/TimelineSection"
 function Home() {
   return (
     <div>
-      {/* Section 1 */}
       <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-white">
-        {/* Become a Instructor Button */}
         <Link to={"/signup"}>
           <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
             <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
@@ -30,21 +25,17 @@ function Home() {
           </div>
         </Link>
 
-        {/* Heading */}
         <div className="text-center text-4xl font-semibold">
           Empower Your Future with
           <HighlightText text={"Coding Skills"} />
         </div>
 
-        {/* Sub Heading */}
         <div className="-mt-3 w-[90%] text-center text-lg font-bold text-richblack-300">
           With our online coding courses, you can learn at your own pace, from
           anywhere in the world, and get access to a wealth of resources,
           including hands-on projects, quizzes, and personalized feedback from
           instructors.
         </div>
-
-        {/* CTA Buttons */}
         <div className="mt-8 flex flex-row gap-7">
           <CTAButton active={true} linkto={"/signup"}>
             Learn More
@@ -53,8 +44,7 @@ function Home() {
             Book a Demo
           </CTAButton>
         </div>
-
-        {/* Video */}
+        
         <div className="mx-3 my-7 shadow-[10px_-5px_50px_-5px] shadow-blue-200">
           <video
             className="shadow-[20px_20px_rgba(255,255,255)]"
@@ -65,8 +55,6 @@ function Home() {
             <source src={Banner} type="video/mp4" />
           </video>
         </div>
-
-        {/* Code Section 1  */}
         <div>
           <CodeBlocks
             position={"lg:flex-row"}
@@ -95,8 +83,6 @@ function Home() {
             backgroundGradient={<div className="codeblock1 absolute"></div>}
           />
         </div>
-
-        {/* Code Section 2 */}
         <div>
           <CodeBlocks
             position={"lg:flex-row-reverse"}
@@ -124,15 +110,11 @@ function Home() {
             backgroundGradient={<div className="codeblock2 absolute"></div>}
           />
         </div>
-
-        {/* Explore Section */}
         <ExploreMore />
       </div>
 
-      {/* Section 2 */}
       <div className="bg-pure-greys-5 text-richblack-700">
         <div className="homepage_bg h-[320px]">
-          {/* Explore Full Catagory Section */}
           <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
             <div className="lg:h-[150px]"></div>
             <div className="flex flex-row gap-7 text-white lg:mt-8">
@@ -150,7 +132,6 @@ function Home() {
         </div>
 
         <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
-          {/* Job that is in Demand - Section 1 */}
           <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
             <div className="text-4xl font-semibold lg:w-[45%] ">
               Get the skills you need for a{" "}
@@ -167,28 +148,20 @@ function Home() {
               </CTAButton>
             </div>
           </div>
-
-          {/* Timeline Section - Section 2 */}
           <TimelineSection />
 
-          {/* Learning Language Section - Section 3 */}
           <LearningLanguageSection />
         </div>
       </div>
 
-      {/* Section 3 */}
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Become a instructor section */}
         <InstructorSection />
-
-        {/* Reviws from Other Learner */}
         <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
         <ReviewSlider />
       </div>
 
-      {/* Footer */}
       <Footer />
     </div>
   )
