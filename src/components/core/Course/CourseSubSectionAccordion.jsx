16861c6fdd362428df3746dac9a94f03083993e1
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-02 00:51:38 +0530

    Update CourseSubSectionAccordion.jsx

--- src/components/core/Course/CourseSubSectionAccordion.jsx
+++ src/components/core/Course/CourseSubSectionAccordion.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react"
 import { AiOutlineDown } from "react-icons/ai"
 import { HiOutlineVideoCamera } from "react-icons/hi"
 
+
 function CourseSubSectionAccordion({ subSec }) {
   return (
     <div>
