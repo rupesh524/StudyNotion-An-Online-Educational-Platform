492bd9f6c333853f621cea332d04a402ba8f08b0
rupesh524 <149881226+rupesh524@users.noreply.github.com>
2025-11-02 00:15:07 +0530

    Update Catalog.jsx

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -10,14 +10,11 @@ import { useSelector } from "react-redux"
 import Error from "./Error"
 
 const Catalog = () => {
-
     const { loading } = useSelector((state) => state.profile)
   const { catalogName } = useParams()
   const [active, setActive] = useState(1)
     const [catalogPageData, setCatalogPageData] = useState(null);
     const [categoryId, setCategoryId] = useState("");
-
-    //Fetch all categories
     useEffect(()=> {
         const getCategories = async() => {
             const res = await apiConnector("GET", categories.CATEGORIES_API);
@@ -59,7 +56,6 @@ const Catalog = () => {
     
       return (
         <>
-          {/* Hero Section */}
           <div className=" box-content bg-richblack-800 px-4">
             <div className="mx-auto flex min-h-[260px] max-w-maxContentTab flex-col justify-center gap-4 lg:max-w-maxContent ">
               <p className="text-sm text-richblack-300">
@@ -77,7 +73,6 @@ const Catalog = () => {
             </div>
           </div>
     
-          {/* Section 1 */}
           <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
             <div className="section_heading">Courses to get you started</div>
             <div className="my-4 flex border-b border-b-richblack-600 text-sm">
@@ -108,7 +103,6 @@ const Catalog = () => {
               />
             </div>
           </div>
-          {/* Section 2 */}
           <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
             <div className="section_heading">
               Top courses in {catalogPageData?.data?.differentCategory?.name}
@@ -119,8 +113,7 @@ const Catalog = () => {
               />
             </div>
           </div>
-    
-          {/* Section 3 */}
+
           <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
             <div className="section_heading">Frequently Bought</div>
             <div className="py-8">
